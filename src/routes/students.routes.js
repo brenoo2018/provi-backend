cbc2ea8ad090903fc2b32a359e9465c61d9b7c82
brenoo2018 <thaynanbreno@gmail.com>
2020-10-06 22:26:26 -0300

    create and update cpf

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -8,6 +8,90 @@ const studentsRouter = Router();
 
 studentsRouter.use(ensureAuthenticated);
 
+studentsRouter.post('/cpf', async (request, response) => {
+  try {
+    const { cpf } = request.body;
+    const { user_uuid } = request.user;
+
+    if (!user_uuid) {
+      return response.status(400).json({ message: 'Uuid not found' });
+    }
+
+    if (!isUuid(user_uuid)) {
+      return response.status(400).json({ message: 'Does not Uuid' });
+    }
+
+    const user = await knex('students')
+      .where({ user_uuid })
+      .first()
+      .orderBy('updated_at', 'desc');
+
+    if (!user) {
+      return response.status(400).json({ message: 'User does not exists' });
+    }
+
+    if (!user.cpf) {
+      await knex('students').insert({
+        uuid: funcUuid(),
+        cpf,
+        user_uuid,
+        created_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+        updated_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+      });
+
+      const aftterUpdate = await knex('students')
+        .where({ user_uuid })
+        .first()
+        .orderBy('updated_at', 'desc');
+
+      return response.json({
+        data: cpf,
+        updated_at: aftterUpdate.updated_at,
+      });
+    }
+
+    if (user.cpf === cpf) {
+      await knex('students')
+        .update({
+          updated_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+        })
+        .where({ uuid: user.uuid, user_uuid });
+
+      const aftterUpdate = await knex('students')
+        .where({ user_uuid })
+        .first()
+        .orderBy('updated_at', 'desc');
+
+      return response.json({
+        data: cpf,
+        updated_at: aftterUpdate.updated_at,
+      });
+    }
+
+    if (user.cpf !== cpf) {
+      await knex('students').insert({
+        uuid: funcUuid(),
+        cpf,
+        user_uuid,
+        created_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+        updated_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+      });
+
+      const aftterUpdate = await knex('students')
+        .where({ user_uuid })
+        .first()
+        .orderBy('updated_at', 'desc');
+
+      return response.json({
+        data: cpf,
+        updated_at: aftterUpdate.updated_at,
+      });
+    }
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
 studentsRouter.post('/full-name', async (request, response) => {
   try {
     const { first_name, last_name } = request.body;
@@ -31,13 +115,14 @@ studentsRouter.post('/full-name', async (request, response) => {
     }
 
     if (!user.first_name && !user.last_name) {
-      await knex('students')
-        .update({
-          first_name,
-          last_name,
-          updated_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
-        })
-        .where({ uuid: user.uuid, user_uuid });
+      await knex('students').insert({
+        uuid: funcUuid(),
+        first_name,
+        last_name,
+        user_uuid,
+        created_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+        updated_at: knex.raw(`strftime('%Y-%m-%d %H:%M:%S', 'now')`),
+      });
 
       const aftterUpdate = await knex('students')
         .where({ user_uuid })
