23ff0698d6e6e4f874906edce6c3238666299ae3
brenoo2018 <thaynanbreno@gmail.com>
2020-10-07 16:20:39 -0300

    test create or update birthday

--- tests/integration/create_update_birthday.spec.js
+++ tests/integration/create_update_birthday.spec.js
@@ -2,20 +2,19 @@ const request = require('supertest');
 const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
-describe('CREATE OR UPDATE FULL NAME', () => {
+describe('CREATE OR UPDATE BIRTHDAY', () => {
   afterAll(async () => {
     await connection.destroy();
   });
-  it('should be able to create or update FULL NAME', async () => {
+  it('should be able to create or update BIRTHDAY', async () => {
     const response = await request(app)
-      .post('/students/full-name')
+      .post('/students/birthday')
       .set(
         'Authorization',
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDIwOTcyMDMsImV4cCI6MTYwMjE4MzYwMywic3ViIjoiZDVmMDA0N2MtYjEzNS00MGJjLTg2YWYtNDk5N2YwOTIzYjY1In0.Z2-LaHC-90nhGHUIjm6MZzj7EMCrIk_AozrTIoM2j-c'
       )
       .send({
-        first_name: 'primeiro nome1',
-        last_name: 'segundo nome1',
+        birthday: '10/12/1998',
       });
 
     expect(response.body).toHaveProperty('data');
